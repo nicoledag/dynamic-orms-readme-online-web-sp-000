7af7a80b8cd3e5af182678e228f6e9cf71c7f610
Nicole D <nnardella@gmail.com>
2019-01-05 14:02:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,13 +39,14 @@ class Song
   end
 
   def save
-      binding.pry
+
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
+          binding.pry
     self.class.table_name
   end
 
